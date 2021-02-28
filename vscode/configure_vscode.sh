45efc87cf27637be88d0422d5c0dff98d90153b7
Fernando Arias <fernan.fpinformatica@gmail.com>
2021-02-28 22:01:21 +0100

    Updated vscode extensions to TS development

--- vscode/configure_vscode.sh
+++ vscode/configure_vscode.sh
@@ -10,18 +10,9 @@ main() {
 function install_extensions() {
     code -v > /dev/null
     if [[ $? -eq 0 ]];then
-        # Javascript
-        code --install-extension dbaeumer.vscode-eslint
-        code --install-extension esbenp.prettier-vscode
-
-        # Vue
-        code --install-extension octref.vetur
-        code --install-extension hollowtree.vue-snippets
-
-        # HTML
-        code --install-extension bradgashler.htmltagwrap
-        code --install-extension formulahendry.auto-close-tag
-        code --install-extension formulahendry.auto-rename-tag
+        # Typescript
+        code --install-extension loiane.ts-extension-pack
+        
         # Utils
         code --install-extension streetsidesoftware.code-spell-checker
         code --install-extension cssho.vscode-svgviewer
@@ -29,7 +20,6 @@ function install_extensions() {
         code --install-extension eamodio.gitlens
         code --install-extension zainchen.json
         code --install-extension pflannery.vscode-versionlens
-        code --install-extension editorconfig.editorconfig
         code --install-extension yzhang.markdown-all-in-one
         # Theme
         code --install-extension dracula-theme.theme-dracula
